0ebfaa8849a30ad729a230c6b6db2282ea339818
Carlos Sanchez <randomouscrap98@aol.com>
2023-09-19 19:55:57 -0400

    Something of an encoder... doesn't work

--- small_encoder/SmallEncoder/SmallEncoder.h
+++ small_encoder/SmallEncoder/SmallEncoder.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <stdint.h>
+
 //"ETX": end of text. Fitting I think. Why not 0? You can't read a compressed string as 
 //a string anyway, so that convention doesn't matter
 constexpr uint8_t DEFAULTDELIMITER = 0x03; //You CAN'T change this, it's part of the super simple encoder!
-constexpr uint8_t WINDOWLENGTH = 32;    //You CAN'T change this; it's part of the super simple encoder
+constexpr uint8_t WINDOWLENGTH = 8;    //You CAN'T change this; it's part of the super simple encoder
+constexpr uint8_t MINMATCHLENGTH = 2;
+constexpr uint8_t MAXMATCHLENGTH = 5;
 
 //The encoding is meant for text, and works as such:
 // - Initialize a sliding window of size 16
@@ -17,21 +21,22 @@ constexpr uint8_t WINDOWLENGTH = 32;    //You CAN'T change this; it's part of th
 // no you're not, go back to sleep
 // no, you don't understand. i must avenge her
 
-#define WINDOWINDEX(x) = ((x) & WINDOWLENGTH - 1)
+// Find the real index into a circular window buffer
+uint8_t windex(uint8_t x)
+{
+    return x & (WINDOWLENGTH - 1);
+}
 
 // Encode text. You generally don't do this on device but figured might as well have it
-uint32_t encode_text(uint8_t * text, uint32_t length, uint8_t * outbuf, uint32_t buflength)
+int32_t encode_text(uint8_t * text, uint32_t length, uint8_t * outbuf, uint32_t buflength)
 {
     uint8_t window[WINDOWLENGTH] = {0};
     uint8_t curwin = 0;
-    //uint8_t * curwin = window; //Circular buffer
-    //uint8_t * curtext = text;
-    uint8_t * curout = outbuf;
     uint32_t outlength = 0;
-
+    uint32_t tp = 0;
 
     //Main compression loop
-    for(uint32_t tp = 0; tp < length; tp++)
+    while(tp < length)
     {
         uint8_t findlen = 0;
         uint8_t findpos = curwin;
@@ -42,14 +47,84 @@ uint32_t encode_text(uint8_t * text, uint32_t length, uint8_t * outbuf, uint32_t
             uint8_t thisfindlen = 0;
 
             //Scan text chars for same 
-            for(thisfindlen = 0; thisfindlen < 5; thisfindlen++)
+            for(thisfindlen = 0; thisfindlen < MAXMATCHLENGTH; thisfindlen++)
             {
-                if(text[tp + thisfindlen] != window[WINDOWINDEX(curwin + wp + thisfindlen)])
-                {
-
-                }
+                if(text[tp + thisfindlen] != window[windex(curwin + wp + thisfindlen)])
+                    break;
             }
+
+            //If we found a greater match, store the parameters
+            if(thisfindlen > findlen)
+            {
+                findlen = thisfindlen;
+                findpos = windex(curwin + wp);
+            }
+
+            //No need to continue if we found the max match
+            if(findlen == MAXMATCHLENGTH)
+                break;
         }
+
+        uint32_t otp = tp;
+
+        //We scanned the window, only store 'compressed value' if enough found
+        if(findlen >= MINMATCHLENGTH)
+        {
+            //high bit for "encoded", 5 bits for scanback, 2 bits for len
+            outbuf[outlength] = 0x80 | (findpos << 2) | (findlen - 2);
+            tp += findlen;
+        }
+        else
+        {
+            outbuf[outlength] = text[tp];
+            tp++;
+        }
+
+        for(otp; otp < tp; otp++)
+            window[windex(curwin++)] = text[otp];
+
+        outlength++;
+
+        if(outlength > buflength)
+            return -1;
     }
+
+    return outlength;
 }
 
+int32_t decode_text(uint8_t * compressed, uint32_t length, uint8_t * outbuf, uint32_t buflength)
+{
+    uint8_t window[WINDOWLENGTH] = {0};
+    uint8_t curwin = 0;
+    int32_t textlen = 0;
+
+    for(uint32_t i = 0; i < length; i++)
+    {
+        uint8_t c = compressed[i];
+
+        uint8_t ocw = curwin;
+
+        //This is an encoded byte.
+        if(c & 0x80)
+        {
+            uint8_t len = MINMATCHLENGTH + (c & 0b11);
+            uint8_t pos = (c >> 2) & 0b11111;
+
+            for(uint8_t wp = 0; wp < len; wp++)
+                window[windex(curwin++)] = window[windex(pos + wp)];
+        }
+        else
+        {
+            window[windex(curwin++)] = c;
+        }
+
+        for(ocw; ocw < curwin; ocw++)
+        {
+            outbuf[textlen++] = window[windex(ocw)];
+            if(textlen > buflength)
+                return -1;
+        }
+    }
+
+    return textlen;
+}
