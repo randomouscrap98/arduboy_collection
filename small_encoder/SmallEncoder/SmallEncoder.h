35e1f3c5f8ac1a36a8c73819a077e6fbdaa07bbd
Carlos Sanchez <randomouscrap98@aol.com>
2023-09-19 20:51:48 -0400

    semi-working new encoder, broken

--- small_encoder/SmallEncoder/SmallEncoder.h
+++ small_encoder/SmallEncoder/SmallEncoder.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <stdint.h>
+#include <algorithm>
 
 //"ETX": end of text. Fitting I think. Why not 0? You can't read a compressed string as 
 //a string anyway, so that convention doesn't matter
 constexpr uint8_t DEFAULTDELIMITER = 0x03; //You CAN'T change this, it's part of the super simple encoder!
-constexpr uint8_t WINDOWLENGTH = 4;    //You CAN'T change this; it's part of the super simple encoder
+constexpr uint8_t WINDOWLENGTH = 32;    //You CAN'T change this; it's part of the super simple encoder
 constexpr uint8_t MINMATCHLENGTH = 2;
 constexpr uint8_t MAXMATCHLENGTH = 5;
 
@@ -21,43 +22,41 @@ constexpr uint8_t MAXMATCHLENGTH = 5;
 // no you're not, go back to sleep
 // no, you don't understand. i must avenge her
 
-// Find the real index into a circular window buffer
-uint8_t windex(uint8_t x)
-{
-    return x & (WINDOWLENGTH - 1);
-}
+//// Find the real index into a circular window buffer
+//uint8_t windex(uint8_t x)
+//{
+//    return x & (WINDOWLENGTH - 1);
+//}
 
 // Encode text. You generally don't do this on device but figured might as well have it
-int32_t encode_text(uint8_t * text, uint32_t length, uint8_t * outbuf, uint32_t buflength)
+int32_t encode_text(uint8_t * text, int32_t length, uint8_t * outbuf, int32_t buflength)
 {
-    uint8_t window[WINDOWLENGTH] = {0};
-    uint8_t curwin = 0;
     uint32_t outlength = 0;
-    uint32_t tp = 0;
+    int32_t tp = 0;
 
     //Main compression loop
     while(tp < length)
     {
         uint8_t findlen = 0;
-        uint8_t findpos = curwin;
+        int32_t findpos = 0;
 
-        //Scan window
-        for(uint8_t wp = 0; wp < WINDOWLENGTH; wp++)
+        //Scan window, which just means scan backwards in text
+        for(int8_t wp = tp - 1; wp >= std::max<int32_t>(0, tp - WINDOWLENGTH); wp--)
         {
             uint8_t thisfindlen = 0;
 
-            //Scan text chars for same 
-            for(thisfindlen = 0; thisfindlen < MAXMATCHLENGTH; thisfindlen++)
+            //Scan forward for the max match, break when nothing found
+            for(uint8_t sp = wp; sp < tp; sp++)
             {
-                if(text[tp + thisfindlen] != window[windex(curwin + wp + thisfindlen)])
-                    break;
+                if(text[tp + thisfindlen] != text[sp] || thisfindlen > MAXMATCHLENGTH) break;
+                thisfindlen++;
             }
 
             //If we found a greater match, store the parameters
             if(thisfindlen > findlen)
             {
                 findlen = thisfindlen;
-                findpos = windex(curwin + wp);
+                findpos = (tp - wp - 1) & 0b11111;
             }
 
             //No need to continue if we found the max match
@@ -65,7 +64,7 @@ int32_t encode_text(uint8_t * text, uint32_t length, uint8_t * outbuf, uint32_t 
                 break;
         }
 
-        uint32_t otp = tp;
+        int32_t otp = tp;
 
         //We scanned the window, only store 'compressed value' if enough found
         if(findlen >= MINMATCHLENGTH)
@@ -80,9 +79,6 @@ int32_t encode_text(uint8_t * text, uint32_t length, uint8_t * outbuf, uint32_t 
             tp++;
         }
 
-        for(otp; otp < tp; otp++)
-            window[windex(curwin++)] = text[otp];
-
         outlength++;
 
         if(outlength > buflength)
@@ -92,40 +88,30 @@ int32_t encode_text(uint8_t * text, uint32_t length, uint8_t * outbuf, uint32_t 
     return outlength;
 }
 
-int32_t decode_text(uint8_t * compressed, uint32_t length, uint8_t * outbuf, uint32_t buflength)
+int32_t decode_text(uint8_t * compressed, int32_t length, uint8_t * outbuf, int32_t buflength)
 {
-    uint8_t window[WINDOWLENGTH] = {0};
-    uint8_t curwin = 0;
     int32_t textlen = 0;
 
     for(uint32_t i = 0; i < length; i++)
     {
         uint8_t c = compressed[i];
-        uint8_t ocw = curwin;
 
         //This is an encoded byte.
         if(c & 0x80)
         {
             uint8_t len = MINMATCHLENGTH + (c & 0b11);
-            uint8_t pos = (c >> 2) & 0b11111;
+            uint8_t back = ((c >> 2) & 0b11111) + 1;
 
             for(uint8_t wp = 0; wp < len; wp++)
             {
-                outbuf[textlen++] = window[windex(ocw)];
-                if(textlen > buflength) return -1;
+                outbuf[textlen] = outbuf[textlen - back];
+                if(++textlen == buflength && wp != len - 1) return -1;
             }
-                //window[windex(curwin++)] = window[windex(pos + wp)];
         }
         else
         {
-            window[windex(curwin++)] = c;
-        }
-
-        for(ocw; ocw < curwin; ocw++)
-        {
-            outbuf[textlen++] = window[windex(ocw)];
-            if(textlen > buflength)
-                return -1;
+            outbuf[textlen] = c;
+            if(++textlen == buflength && i != length - 1) return -1;
         }
     }
 
--- small_encoder/SmallEncoder/main.cpp
+++ small_encoder/SmallEncoder/main.cpp
@@ -17,6 +17,7 @@ void runtest(const char * text)
     uint32_t encodedlen = encode_text((uint8_t *)text, textlen, buffer, MAXBUFFERLENGTH);
     uint32_t decodedlen = decode_text(buffer, encodedlen, (uint8_t *)outtext, MAXBUFFERLENGTH);
     printf("Original text length: %d, encoded: %d, decoded: %d\n", textlen, encodedlen, decodedlen);
+    //printf("Original text length: %d, encoded: %d\n", textlen, encodedlen);
 
     #ifdef SHOWTEXT
     printf(" > %s\n", text);
@@ -32,10 +33,10 @@ int main()
         printf("Char is wrong size: %d\n", sizeof(char));
     }
 
-    //runtest("This sure is some text, yessir. It would be a shame if it were to compress poorly.");
-    //runtest("bacon bacon bacon bacon eggs eggs eggs eggs bacon bacon bacon bacon yeah");
+    runtest("This sure is some text, yessir. It would be a shame if it were to compress poorly.");
+    runtest("bacon bacon bacon bacon eggs eggs eggs eggs bacon bacon bacon bacon yeah");
     runtest("You have come to the right place. I will teach you");
-    //runtest("OK so let's try this with a lot of text\nYou see, there will often be a lot of text next to each other\nI'm hoping the dang system will give SOME level of compression\nEven though I know more complex compression schemes would produce significantly better results\nOh well");
+    runtest("OK so let's try this with a lot of text\nYou see, there will often be a lot of text next to each other\nI'm hoping the dang system will give SOME level of compression\nEven though I know more complex compression schemes would produce significantly better results\nOh well");
 
     printf("Done\n");
 }
